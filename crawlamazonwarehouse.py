2aa8c6fc0fb4630470c96cce4070b313355beffc
ly2xxx <ly2xxx@hotmail.com>
2024-02-10 01:25:56 +0000

    fully working amazon warehouse scrape

--- crawlamazonwarehouse.py
+++ crawlamazonwarehouse.py
@@ -26,22 +26,34 @@ def get_title(soup):
 	return title_string
 
 # Function to extract Product Price
-def get_discount_ratio(soup):
+def get_price(soup):
 
 	try:
-		new_price = soup.find("span", attrs={'class':'a-offscreen'}).string.strip()
-		print("New = ", new_price)
-		# prices = soup.select('div.a-box-inner span.a-offscreen')
-		# used_price = prices[0].get_text().strip()
-		used_price = soup.find("div",attrs={'data-csa-c-buying-option-type':'USED'}).string.strip()
-		print("Used = ", used_price)
-		price = float(used_price[1:]) / float(new_price[1:])
-		if (price < 0.6):
-			print("BARGAIN!!!")
+		price = soup.find("span", attrs={'class':'a-offscreen'}).string.strip()
+
 	except AttributeError:
 		price = ""	
 
 	return price
+
+# # Function to extract Product Price
+# def get_discount_ratio(soup):
+
+# 	try:
+# 		new_price = soup.find("span", attrs={'class':'a-offscreen'}).string.strip()
+# 		print("New = ", new_price)
+# 		# prices = soup.select('span."a-price aok-align-center centralizedApexPricePriceToPayMargin" span.a-offscreen')
+# 		prices = soup.find("span", attrs={'class':'a-price aok-align-center centralizedApexPricePriceToPayMargin'})
+# 		used_price = prices[0].get_text().strip()
+# 		# used_price = soup.find("div",attrs={'data-csa-c-buying-option-type':'USED'}).string.strip()
+# 		print("Used = ", used_price)
+# 		price = float(used_price[1:]) / float(new_price[1:])
+# 		if (price < 0.6):
+# 			print("BARGAIN!!!")
+# 	except AttributeError:
+# 		price = ""	
+
+# 	return price
 
 # Function to extract Product Rating
 def get_rating(soup):
@@ -95,23 +107,28 @@ def print_product_info(url):
 	# soup = BeautifulSoup(webpage.content, "lxml")
 	soup = BeautifulSoup(webpage.content, "html.parser")
 
+	price = get_price(soup)
+
 	# Function calls to display all necessary product information
 	print("Product Title =", get_title(soup))
-	print("Product Discount =", get_discount_ratio(soup))
+	print("Product Price =", price)
 	print("Product Rating =", get_rating(soup))
 	print("Number of Product Reviews =", get_review_count(soup))
 	print("Availability =", get_availability(soup))
 	print()
-	print()
+
+	return price
 
 if __name__ == '__main__':
 
 	#https://www.octoparse.com/blog/how-to-scrape-amazon-data-using-python
+	#c:/code/py_playground/.venv/Scripts/python.exe c:/code/py_playground/crawlamazonwarehouse.py >> results\golf.txt
 	headers2 = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
 	domain_url = "https://www.amazon.co.uk"
-	base_url = "https://www.amazon.co.uk/s?k=ddr4+ram+32gb&i=warehouse-deals&page="
-	for page_number in range(10, 101):
+	# base_url = "https://www.amazon.co.uk/s?k=ddr4+ram+32gb&i=warehouse-deals&page="
+	base_url = "https://www.amazon.co.uk/s?k=golf&i=warehouse-deals&page="
+	for page_number in range(1, 101):
 		# Create the new URL by replacing the page number
 		page_url = base_url + str(page_number)
 		print(page_url)
@@ -125,11 +142,24 @@ if __name__ == '__main__':
 			break
 		for title in titles:
 			print (title.get_text())
-			href = title.find('a', attrs={'class':'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}).get("href")
-			print (href)
+			item = title.find('a', attrs={'class':'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
+			href = item.get("href")
+			# print (href)
 			# Split the string using '/'
 			parts = href.split('/')
-			print_product_info(domain_url+"/dp/"+parts[3])
+			new_url = domain_url+"/dp/"+parts[3]
+			used_url = domain_url+"/"+href
+			new_price = print_product_info(new_url)
+			used_price = print_product_info(used_url)
+			discount = float(used_price[1:]) / float(new_price[1:])
+			print("Price discount =", discount)
+			if (discount < 0.6):
+				print("BARGAIN!!!")
+
+			print()
+			print()
+			print()
+			print()
 
  	# titles = [title.get_text() for title in titles]
 
